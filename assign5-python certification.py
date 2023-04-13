7da8c2985eb4185312f4e6354840edf0d94f6ec2
Rishav Raj <38533962+rishavasitis@users.noreply.github.com>
2023-04-13 19:10:18 +0530

    Add files via upload

---
+++ assign5-python certification.py
@@ -0,0 +1,34 @@
+#1 XML 1 - Find the Score
+import sys
+import xml.etree.ElementTree as etree
+def get_attr_number(node):
+    # your code goes here
+    return len(node.attrib) + sum(get_attr_number(child) for child in node);
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
+
+# 2 XML2 - Find the Maximum Depth
+
+
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    if (level == maxdepth):
+        maxdepth += 1
+    for child in elem:
+        depth(child, level + 1)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
